e64086c99cd1b445b4e6f88ad0cb4b5a14bb1cbd
heym <10216908@qq.com>
2017-11-10 12:50:09 +0800

    bf

--- src/crawl_win.py
+++ src/crawl_win.py
@@ -1,134 +1,72 @@
 # coding=utf-8
 from selenium import webdriver
 import time
-import xlrd
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import json
+import requests
 import pymysql
 import uuid
 import sys
+from datetime import datetime
+
+from selenium.common.exceptions import NoSuchElementException
 
 
 def login():
     old_driver = webdriver.Chrome()
     old_driver.get("https://www.tianyancha.com/login")
-
+    time.sleep(1.3)
+    old_driver.implicitly_wait(10)
     # 模拟登陆
     old_driver.find_element_by_xpath(
         '//*[@id="web-content"]/div/div/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/input').send_keys("18761671816")
     old_driver.find_element_by_xpath(
         '//*[@id="web-content"]/div/div/div/div[2]/div/div[2]/div[2]/div[2]/div[3]/input').send_keys("123456abc")
+    time.sleep(0.3)
     old_driver.find_element_by_xpath(
         '//*[@id="web-content"]/div/div/div/div[2]/div/div[2]/div[2]/div[2]/div[5]').click()
     time.sleep(0.14)
     old_driver.refresh()
+    old_driver.get('https://www.tianyancha.com/company/11480443')
     return old_driver
 
 
-def driver_open():
-    dcap = dict(DesiredCapabilities.PHANTOMJS)
-    dcap["phantomjs.page.settings.userAgent"] = (
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Mobile/9B176 MicroMessenger/4.3.2"
-    )
-    service_args = []
-    service_args.append('--load-images=no')  ##关闭图片加载
-    service_args.append('--disk-cache=yes')  ##开启缓存
-    service_args.append('--ignore-ssl-errors=true')  ##忽略https错误
-    driver = webdriver.PhantomJS(executable_path='/usr/local/bin/phantomjs', desired_capabilities=dcap,
-                                 service_args=service_args)
-    return driver
-
-
-def open_excel(file):
-    try:
-        book = xlrd.open_workbook(file)
-        return book
-    except Exception as e:
-        print ('打开工作簿' + file + '出错：' + str(e))
-
-
-# 读取工作簿中所有工作表
-def read_sheets(file):
-    try:
-        book = open_excel(file)
-        sheets = book.sheets()
-        return sheets
-    except Exception as e:
-        print ('读取工作表出错：' + str(e))
-
-
-# 读取某一工作表中数据某一列的数据
-def read_data(sheet, n=0):
-    dataset = []
-    for r in range(sheet.nrows):
-        col = sheet.cell(r, n).value
-        dataset.append(col)
-    return dataset
-
-
-def load_excel(logfile, company_file):
-    now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-    with open(logfile, 'a') as f:
-        f.write('\n当前时间：' + now + '\n')
-    sheets = read_sheets(company_file)
-    cornames = []
-    for sheet in sheets:
-        dataset = read_data(sheet)
-        cornames.extend(dataset)
-    print "一共获取到" + bytes(len(cornames)) + "个企业"
-    return cornames
-
-
-def crawl(name, driver):
+def crawl(name, driver, quest_id):
     print '开始》》》》》》》' + name
-    #  切换到新的窗口
-    handles = driver.window_handles
-    driver.switch_to_window(handles[-1])
-    time.sleep(0.089)
-    driver.get("https://www.tianyancha.com/")
-    time.sleep(0.78)
-
     try:
-        driver.find_element_by_xpath('//*[@id="home-main-search"]').send_keys(name)  # 输入名字
-        time.sleep(2.34)
-        driver.find_element_by_xpath(".//*[@class='input-group-addon search_button']").click()  # 点击搜索
-        driver.implicitly_wait(10)
+        driver.find_element_by_xpath('//*[@id="header-company-search"]').clear()  # 清除文本
+        driver.find_element_by_xpath('//*[@id="header-company-search"]').send_keys(name.decode('utf-8'))  # 输入名字
+        time.sleep(1.34)
+        driver.find_element_by_xpath('//*[@id="web-header"]/div/div/div[1]/div[2]/div[2]/div[1]/div').click()  # 点击搜索
+        driver.implicitly_wait(15)
         time.sleep(0.12)
-        driver.find_element_by_xpath('//*[@id="searchTogal"]').click()  # 收起
+        # driver.find_element_by_xpath('//*[@id="searchTogal"]').click()  # 收起
         time.sleep(0.21)
-        # 选择相关度最高的搜索结果 第一条搜索框
-        tag = driver.find_elements_by_xpath("//div[@class='search_right_item']")
-        if len(tag) == 0:
+        # 选择相关度最高的搜索结果 第一条搜索结果
+        try:
+            detail_tag = driver.find_element_by_xpath(
+                '//div[@class="search_right_item"]/div[1]/a')
+        except NoSuchElementException, e:
+            no_result_match(quest_id)
             return None
-        tag[0].find_element_by_tag_name('a').click()
+        real_name = detail_tag.text  # 真实名字
+        detail_url = detail_tag.get_attribute('href')
         driver.implicitly_wait(5)
-        # 转化句柄
-        now_handle = driver.current_window_handle
-        all_handles = driver.window_handles
-        for handle in all_handles:
-            if handle != now_handle:
-                # 输出待选择的窗口句柄
-                # print(handle)
-                driver.switch_to.window(handle)
+        driver.get(detail_url)  # 获取详情页面
+        driver.implicitly_wait(5)
         time.sleep(0.09)
 
         # 解析
         base = driver.find_element_by_xpath("//div[@class='company_header_width ie9Style']/div")
-        name = base.text.split('浏览')[0]
         tel = base.text.split('电话：')[1].split('邮箱：')[0]
-        liulan = base.text.split('浏览')[1].split('\n')[0]
         email = base.text.split('邮箱：')[1].split('\n')[0]
         web = base.text.split('网址：')[1].split('地址')[0]
         address = base.text.split('地址：')[1]
-        # abstract = driver.find_element_by_xpath("//div[@class='sec-c2 over-hide']//script")
         tabs = driver.find_elements_by_tag_name('table')
         rows = tabs[1].find_elements_by_tag_name('tr')
-        cols = rows[0].find_elements_by_tag_name('td' and 'th')
         # 工商注册号
         reg_code = rows[0].find_elements_by_tag_name('td')[1].text
         # 注册地址
         reg_address = rows[5].find_elements_by_tag_name('td')[1].text
-        # 英文名称
-        english_name = rows[5].find_elements_by_tag_name('td')[1].text
         # 经营范围
         ent_range = rows[6].find_elements_by_tag_name('td')[1].text
         # 统一信用代码
@@ -156,28 +94,53 @@ def crawl(name, driver):
         # 登记机关
         regi_unit = driver.find_element_by_xpath(
             '//*[@id="_container_baseInfo"]/div/div[2]/table/tbody/tr[5]/td[2]').text
-
-        based_info = (idd, name, tel, email, web, address, reg_code, reg_address, english_name, ent_range,
-                      creditcode, tax_code, deadline, ent_type, name, frdb, zczb, ent_status, regi_date, hz_date,
-                      regi_unit)
-        driver.close()
-    except Exception:
-        print '解析出错======='
-        handles = driver.window_handles
-        if len(handles) > 1:
-            driver.close()
+        now_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        if real_name == name:
+            is_match = 'Y'
+        else:
+            is_match = 'N'
+        based_info = (real_name, name, quest_id, tel, email, web, address, reg_code, reg_address, ent_range,
+                      creditcode, tax_code, deadline, ent_type, frdb, zczb, ent_status, regi_date, hz_date,
+                      regi_unit, is_match, now_time)
+    except Exception, e:
+        print e.message
+        error_quest(quest_id)
         return None
     return based_info
 
 
-def save_data(table):
+def save_data(table, quest_id):
     conn = pymysql.connect(host='wentuotuo.com', user='root', passwd='root', db='tianyanchatest', port=3306,
                            charset='utf8')
     cur = conn.cursor()  # 获取一个游标
-    sql = "INSERT INTO gys (id, name, entPhone, entEmail, ent_url, ent_address," \
-          " ent_reg_no,ent_reg_address, ent_english_name, ent_range, credit_code, tax_person_code, entDeadline, " \
-          "ent_type, raw_name,frdb, zczb, ent_status, regi_date, hz_date, regi_unit) VALUES ( '%s','%s','%s','%s','%s','%s','%s', '%s', '%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s' )"
+    sql = "INSERT INTO ent_info (real_name, raw_name, quest_id, entPhone," \
+          " entEmail,ent_url, ent_address, ent_reg_no, ent_reg_address, ent_range, credit_code, tax_person_code, entDeadline, " \
+          "ent_type, ent_represent, regi_capital, ent_status, regi_date, hz_date, regi_unit, is_match, input_time) VALUES ( '%s','%s','%s','%s','%s','%s','%s', '%s', '%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s' )"
     cur.execute(sql % table)
+    sql2 = "update quest set status='finish' where quest_id='" + quest_id + "'"  # 没匹配到
+    cur.execute(sql2)
+    conn.commit()
+    cur.close()  # 关闭游标
+    conn.close()  # 释放数据库资源
+
+
+def error_quest(questid):
+    conn = pymysql.connect(host='wentuotuo.com', user='root', passwd='root', db='tianyanchatest', port=3306,
+                           charset='utf8')
+    cur = conn.cursor()  # 获取一个游标
+    sql = "update quest set status='error' where quest_id='" + questid + "'"  # 没匹配到
+    cur.execute(sql)
+    conn.commit()
+    cur.close()  # 关闭游标
+    conn.close()  # 释放数据库资源
+
+
+def no_result_match(questid):
+    conn = pymysql.connect(host='wentuotuo.com', user='root', passwd='root', db='tianyanchatest', port=3306,
+                           charset='utf8')
+    cur = conn.cursor()  # 获取一个游标
+    sql = "update quest set status='nmfinish' where quest_id='" + questid + "'"  # 没匹配到
+    cur.execute(sql)
     conn.commit()
     cur.close()  # 关闭游标
     conn.close()  # 释放数据库资源
@@ -187,16 +150,26 @@ if __name__ == '__main__':
     reload(sys)
     sys.setdefaultencoding('utf-8')
     logfile = 'failed_log.txt'
-    filename = 'D:\Workspace\Pyworkspace\\tyc_spider\data\last12785list.xlsx'
-    # 登录
-    old_driver = login()
-    # 读取excel
-    cornames = load_excel(logfile, filename)
-    # 爬取网站
-    for i in range(len(cornames)):
-        name = cornames[i].strip()
-        info = crawl(name, old_driver)
-        if info is None:
-            continue
-        # 判断是否有信息了 todo
-        save_data(info)
+    try:
+        # 登录
+        old_driver = login()
+    except Exception:
+        print '登录出错'
+    while 1:
+        # 获取任务
+        r = requests.get("http://10.1.1.157:8011/getquest").text
+        req = r.encode('utf-8')
+        quest_list = json.loads(r, encoding='utf-8')
+        # quest = "43shiu2872834;万达;陈仁飞;大神;王老吉"
+        if len(quest_list)==0:
+            print '没有任务,休眠5秒'
+            time.sleep(5)
+        for quest in quest_list:
+            quest_id = quest["quest_id"]
+            ent_name = quest["ent_name"]
+            # 爬取网站
+            info = crawl(ent_name, old_driver, quest_id)
+            if info is None:
+                continue
+            # 判断是否有信息了 todo
+            save_data(info, quest_id)
---
+++ src/load_ent.py
@@ -0,0 +1,79 @@
+# coding=utf-8
+import sys
+import uuid
+from datetime import datetime
+import pymysql
+import xlrd
+
+
+def open_excel(file):
+    try:
+        book = xlrd.open_workbook(file)
+        return book
+    except Exception as e:
+        print ('打开工作簿' + file + '出错：' + str(e))
+
+
+# 读取工作簿中所有工作表
+def read_sheets(file):
+    try:
+        book = open_excel(file)
+        sheets = book.sheets()
+        return sheets
+    except Exception as e:
+        print ('读取工作表出错：' + str(e))
+
+
+# 读取某一工作表中数据某一列的数据
+def read_data(sheet, n=0):
+    dataset = []
+    for r in range(sheet.nrows):
+        col = sheet.cell(r, n).value
+        dataset.append(col)
+    return dataset
+
+
+def load_excel(company_file):
+    sheets = read_sheets(company_file)
+    cornames = []
+    for sheet in sheets:
+        dataset = read_data(sheet)
+        cornames.extend(dataset)
+    return cornames
+
+
+def save_data(cornames):
+    j = 0
+    many_values = []
+    status = 'waiting'
+    conn = pymysql.connect(host='wentuotuo.com', user='root', passwd='root', db='tianyanchatest', port=3306,
+                           charset='utf8')
+    cur = conn.cursor()  # 获取一个游标
+    sql = "INSERT INTO quest (quest_id, ent_name, status, input_time) VALUES ( %s,%s,%s,%s)"
+    for i in range(len(cornames)):
+        name = cornames[i].strip()
+        now_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        idd = str(uuid.uuid1())
+        idd.replace('-', '')
+        values = (idd, name, status, now_time)
+        many_values.append(values)
+        j = j + 1
+        if j % 1000 == 0:
+            print str(i + 1) + ' === ' + str(name)
+            cur.executemany(sql, many_values)
+            many_values = []
+            conn.commit()
+            j = 0
+    if j != 0:
+        cur.executemany(sql, many_values)
+        conn.commit()
+    cur.close()  # 关闭游标
+    conn.close()  # 释放数据库资源
+
+
+if __name__ == '__main__':
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    filename = 'D:\Workspace\Pyworkspace\\tyc_spider\data\gysraw.xlsx'
+    cornames = load_excel(filename)
+    save_data(cornames)
